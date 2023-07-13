2b8790d1319385bc1c20135724d6ec23def24f41
Brandon Tassone <brandontassone@gmail.com>
2023-07-13 14:20:23 -0400

    Added test routes for with and without paths to all bucket policies

--- iam/iam_test.go
+++ iam/iam_test.go
@@ -110,7 +110,7 @@ func TestReadOnlyBucketPolicyWithPath(t *testing.T) {
 	}
 }
 
-func TestReadWriteBucketPolicy(t *testing.T) {
+func TestReadWriteBucketPolicyWithoutPath(t *testing.T) {
 	expected := `{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Action":["s3:GetAccelerateConfiguration","s3:GetBucketAcl","s3:GetBucketCORS","s3:GetBucketLocation","s3:GetBucketLogging","s3:GetBucketNotification","s3:GetBucketObjectLockConfiguration","s3:GetBucketPolicy","s3:GetBucketPolicyStatus","s3:GetBucketPublicAccessBlock","s3:GetBucketRequestPayment","s3:GetBucketTagging","s3:GetBucketVersioning","s3:GetBucketWebsite","s3:GetEncryptionConfiguration","s3:GetInventoryConfiguration","s3:GetLifecycleConfiguration","s3:GetReplicationConfiguration","s3:GetMetricsConfiguration","s3:GetReplicationConfiguration","s3:ListAccessPoints","s3:ListAllMyBuckets","s3:ListBucket","s3:ListBucketMultipartUploads","s3:ListBucketVersions","s3:ListMultipartUploadParts"],"Resource":["arn:aws:s3:::vehicles"],"Condition":null},{"Effect":"Allow","Action":["s3:GetObject","s3:GetObjectAcl","s3:GetObjectLegalHold","s3:GetObjectRetention","s3:GetObjectTagging","s3:GetObjectVersion","s3:GetObjectVersionAcl","s3:GetObjectVersionForReplication","s3:GetObjectVersionTagging"],"Resource":["arn:aws:s3:::vehicles/*"],"Condition":null},{"Effect":"Allow","Action":["s3:AbortMultipartUpload","s3:DeleteObject","s3:DeleteObjectVersion","s3:PutObject","s3:PutObjectAcl","s3:PutObjectVersionAcl","s3:PutObjectRetention","s3:ReplicateDelete","s3:ReplicateObject","s3:RestoreObject","s3:PutObjectLegalHold"],"Resource":["arn:aws:s3:::vehicles/*"],"Condition":null}]}`
 
 	policyBytes, err := i.ReadWriteBucketPolicy(bucket, "")
@@ -123,9 +123,34 @@ func TestReadWriteBucketPolicy(t *testing.T) {
 	}
 }
 
-func TestAdminBucketPolicy(t *testing.T) {
+func TestReadWriteBucketPolicyWithPath(t *testing.T) {
+	expected := `{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Action":["s3:GetAccelerateConfiguration","s3:GetBucketAcl","s3:GetBucketCORS","s3:GetBucketLocation","s3:GetBucketLogging","s3:GetBucketNotification","s3:GetBucketObjectLockConfiguration","s3:GetBucketPolicy","s3:GetBucketPolicyStatus","s3:GetBucketPublicAccessBlock","s3:GetBucketRequestPayment","s3:GetBucketTagging","s3:GetBucketVersioning","s3:GetBucketWebsite","s3:GetEncryptionConfiguration","s3:GetInventoryConfiguration","s3:GetLifecycleConfiguration","s3:GetReplicationConfiguration","s3:GetMetricsConfiguration","s3:GetReplicationConfiguration","s3:ListAccessPoints","s3:ListAllMyBuckets","s3:ListBucket","s3:ListBucketMultipartUploads","s3:ListBucketVersions","s3:ListMultipartUploadParts"],"Resource":["arn:aws:s3:::vehicles"],"Condition":null},{"Effect":"Allow","Action":["s3:GetObject","s3:GetObjectAcl","s3:GetObjectLegalHold","s3:GetObjectRetention","s3:GetObjectTagging","s3:GetObjectVersion","s3:GetObjectVersionAcl","s3:GetObjectVersionForReplication","s3:GetObjectVersionTagging"],"Resource":["arn:aws:s3:::vehicles/testdata/*"],"Condition":null},{"Effect":"Allow","Action":["s3:AbortMultipartUpload","s3:DeleteObject","s3:DeleteObjectVersion","s3:PutObject","s3:PutObjectAcl","s3:PutObjectVersionAcl","s3:PutObjectRetention","s3:ReplicateDelete","s3:ReplicateObject","s3:RestoreObject","s3:PutObjectLegalHold"],"Resource":["arn:aws:s3:::vehicles/testdata/*"],"Condition":null}]}`
+
+	policyBytes, err := i.ReadWriteBucketPolicy(bucket, "testdata")
+	if err != nil {
+		t.Errorf("expected ReadWriteBucketPolicy to return nil error, got %s", err)
+	}
+
+	if !bytes.Equal(policyBytes, []byte(expected)) {
+		t.Errorf("expected: %s\ngot: %s", expected, policyBytes)
+	}
+}
+
+func TestAdminBucketPolicyWithoutPath(t *testing.T) {
 	expected := `{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Action":["s3:PutBucketPolicy","s3:DeleteBucketPolicy","s3:PutBucketWebsite","s3:DeleteBucketWebsite","s3:ListAllMyBuckets","s3:PutAccelerateConfiguration","s3:PutBucketAcl","s3:PutBucketCORS","s3:PutBucketNotification","s3:PutBucketObjectLockConfiguration","s3:PutBucketRequestPayment","s3:PutBucketVersioning","s3:PutInventoryConfiguration","s3:PutLifecycleConfiguration","s3:PutReplicationConfiguration"],"Resource":["arn:aws:s3:::vehicles"],"Condition":null},{"Effect":"Allow","Action":["s3:GetAccelerateConfiguration","s3:GetBucketAcl","s3:GetBucketCORS","s3:GetBucketLocation","s3:GetBucketLogging","s3:GetBucketNotification","s3:GetBucketObjectLockConfiguration","s3:GetBucketPolicy","s3:GetBucketPolicyStatus","s3:GetBucketPublicAccessBlock","s3:GetBucketRequestPayment","s3:GetBucketTagging","s3:GetBucketVersioning","s3:GetBucketWebsite","s3:GetEncryptionConfiguration","s3:GetInventoryConfiguration","s3:GetLifecycleConfiguration","s3:GetReplicationConfiguration","s3:GetMetricsConfiguration","s3:GetReplicationConfiguration","s3:ListAccessPoints","s3:ListAllMyBuckets","s3:ListBucket","s3:ListBucketMultipartUploads","s3:ListBucketVersions","s3:ListMultipartUploadParts"],"Resource":["arn:aws:s3:::vehicles"],"Condition":null},{"Effect":"Allow","Action":["s3:GetObject","s3:GetObjectAcl","s3:GetObjectLegalHold","s3:GetObjectRetention","s3:GetObjectTagging","s3:GetObjectVersion","s3:GetObjectVersionAcl","s3:GetObjectVersionForReplication","s3:GetObjectVersionTagging"],"Resource":["arn:aws:s3:::vehicles/*"],"Condition":null},{"Effect":"Allow","Action":["s3:AbortMultipartUpload","s3:DeleteObject","s3:DeleteObjectVersion","s3:PutObject","s3:PutObjectAcl","s3:PutObjectVersionAcl","s3:PutObjectRetention","s3:ReplicateDelete","s3:ReplicateObject","s3:RestoreObject","s3:PutObjectLegalHold"],"Resource":["arn:aws:s3:::vehicles/*"],"Condition":null}]}`
 	policyBytes, err := i.AdminBucketPolicy(bucket, "")
+	if err != nil {
+		t.Errorf("expected AdminBucketPolicy to return nil error, got %s", err)
+	}
+
+	if !bytes.Equal(policyBytes, []byte(expected)) {
+		t.Errorf("expected: %s\ngot: %s", expected, policyBytes)
+	}
+}
+
+func TestAdminBucketPolicyWithPath(t *testing.T) {
+	expected := `{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Action":["s3:PutBucketPolicy","s3:DeleteBucketPolicy","s3:PutBucketWebsite","s3:DeleteBucketWebsite","s3:ListAllMyBuckets","s3:PutAccelerateConfiguration","s3:PutBucketAcl","s3:PutBucketCORS","s3:PutBucketNotification","s3:PutBucketObjectLockConfiguration","s3:PutBucketRequestPayment","s3:PutBucketVersioning","s3:PutInventoryConfiguration","s3:PutLifecycleConfiguration","s3:PutReplicationConfiguration"],"Resource":["arn:aws:s3:::vehicles"],"Condition":null},{"Effect":"Allow","Action":["s3:GetAccelerateConfiguration","s3:GetBucketAcl","s3:GetBucketCORS","s3:GetBucketLocation","s3:GetBucketLogging","s3:GetBucketNotification","s3:GetBucketObjectLockConfiguration","s3:GetBucketPolicy","s3:GetBucketPolicyStatus","s3:GetBucketPublicAccessBlock","s3:GetBucketRequestPayment","s3:GetBucketTagging","s3:GetBucketVersioning","s3:GetBucketWebsite","s3:GetEncryptionConfiguration","s3:GetInventoryConfiguration","s3:GetLifecycleConfiguration","s3:GetReplicationConfiguration","s3:GetMetricsConfiguration","s3:GetReplicationConfiguration","s3:ListAccessPoints","s3:ListAllMyBuckets","s3:ListBucket","s3:ListBucketMultipartUploads","s3:ListBucketVersions","s3:ListMultipartUploadParts"],"Resource":["arn:aws:s3:::vehicles"],"Condition":null},{"Effect":"Allow","Action":["s3:GetObject","s3:GetObjectAcl","s3:GetObjectLegalHold","s3:GetObjectRetention","s3:GetObjectTagging","s3:GetObjectVersion","s3:GetObjectVersionAcl","s3:GetObjectVersionForReplication","s3:GetObjectVersionTagging"],"Resource":["arn:aws:s3:::vehicles/testdata/*"],"Condition":null},{"Effect":"Allow","Action":["s3:AbortMultipartUpload","s3:DeleteObject","s3:DeleteObjectVersion","s3:PutObject","s3:PutObjectAcl","s3:PutObjectVersionAcl","s3:PutObjectRetention","s3:ReplicateDelete","s3:ReplicateObject","s3:RestoreObject","s3:PutObjectLegalHold"],"Resource":["arn:aws:s3:::vehicles/testdata/*"],"Condition":null}]}`
+	policyBytes, err := i.AdminBucketPolicy(bucket, "testdata")
 	if err != nil {
 		t.Errorf("expected AdminBucketPolicy to return nil error, got %s", err)
 	}
