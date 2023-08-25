bbfc9055b40d4cc311c1db68a015d934c6dfffb3
bt353 <brandon.tassone@yale.edu>
2023-08-25 16:47:38 -0400

    Remove test file temporarily

--- s3/lifecycle.go
+++ s3/lifecycle.go
@@ -3,8 +3,6 @@ package s3
 import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
-	log "github.com/sirupsen/logrus"
-	"time"
 )
 
 var (
@@ -41,14 +39,4 @@ func (l *SupportedLifecycles) GetLifecycle(lifecycle string) *s3.LifecycleRule {
 	}
 
 	return &lifecycleRule
-}
-
-// getMidnightTomorrow gets the current time, adds a day, then sets it to midnight
-func getMidnightTomorrow() time.Time {
-	sometimeTomorrow := time.Now().Add(time.Hour * 24)
-
-	out, _ := time.Parse(time.RFC3339, sometimeTomorrow.Format("2006-01-02"))
-	log.Info("tomorrow midnight: %s", out.String())
-
-	return out
 }
